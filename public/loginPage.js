c4cd6b02421173c814ac0d37d025d2eb3556fa5a
galilinka <gali_82@mail.ru>
2021-10-05 20:11:23 +0300

    first commit variables

---
+++ public/loginPage.js
@@ -0,0 +1,17 @@
+"use strict";
+class UserForm {
+    constructor (){
+        this.loginFormCallback = loginFormAction(date);
+        this.registerFormCallback = registerFormAction(date);
+    }
+    loginFormAction()
+}
+//const userForm = new UserForm();
+class ApiConnector {
+    constructor (){
+    }
+    login({login, password}, callback){
+    }
+    register({login, password}, callback){
+    }
+}

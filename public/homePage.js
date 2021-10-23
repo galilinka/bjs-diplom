7eea200deeda621ed989b11a3043805a04a1b2b0
galilinka <gali_82@mail.ru>
2021-10-23 12:01:51 +0300

    10th variables

--- public/homePage.js
+++ public/homePage.js
@@ -16,18 +16,25 @@ ApiConnector.current((response) => {
         ProfileWidget.showProfile(response.data);
     }
 });
-//получение текущих курсов валют
+
 const ratesBoard = new RatesBoard();
-let func = ((response) => {
-    console.log(response);
-    if (response.success === true){
-         ratesBoard.clearTable();
-         ratesBoard.fillTable(response.data);
-    }
-});
- const interval = setInterval(() => {
-    ApiConnector.getStocks();
- }, 60000)
+
+function getRates() {
+    ApiConnector.getStocks((response) => {
+        console.log(response);
+        if (response.success === true) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+          };
+    });
+  }
+
+  getRates();
+
+  setInterval(getRates, 60000);
+
+
+
 //операции с деньгами
 const moneyManager = new MoneyManager();
 //пополнение баланса
@@ -35,13 +42,41 @@ moneyManager.addMoneyCallback = (data) => {
     ApiConnector.addMoney(data, (response) => {
         console.log(response);
         if (response.success === true){
-            showProfile(response.data);
+            ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(true, 'Баланс пополнен');
         } else {
-            moneyManager.setMessage(false, response.e);
+            moneyManager.setMessage(false, response.error);
         }
     });
 }
+
+//конвертирование валюты
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, (response) =>{
+        console.log(response);
+        if (response.success === true){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(true, 'Операция завершена успешно!');
+        } else {
+            moneyManager.setMessage(false, response.error);
+        }
+    })
+}
+
+//перевод валюты
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, (response) =>{
+        console.log(response);
+        if (response.success === true){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(true, 'Операция завершена успешно!');
+        } else {
+            moneyManager.setMessage(false, response.error);
+        }
+    })
+}
+
+
 //работа с избранным
  const favoritesWidget = new FavoritesWidget();
  //начальный список избранного
@@ -54,7 +89,37 @@ moneyManager.addMoneyCallback = (data) => {
      }
 });
 
+//добавление пользователя в список избранных
+ favoritesWidget.addUserCallback = (data) => {
+     ApiConnector.addUserToFavorites(data, (response) => {
+            console.log(response);
+            if (response.success === true){
+                favoritesWidget.clearTable();
+                favoritesWidget.fillTable(response.data);
+                moneyManager.updateUsersList(response.data);
+                favoritesWidget.setMessage(true, 'Операция завершена успешно!');
+             } else {
+                favoritesWidget.setMessage(false, response.error);
+             }
+     })
+ }
+
+ //удаление пользователя из избранного
+ favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+           console.log(response);
+           if (response.success === true){
+               favoritesWidget.clearTable();
+               favoritesWidget.fillTable(response.data);
+               moneyManager.updateUsersList(response.data);
+               favoritesWidget.setMessage(true, 'Операция завершена успешно!');
+            } else {
+               favoritesWidget.setMessage(false, response.error);
+            }
+    })
+}
 
 
+ 
 
 
